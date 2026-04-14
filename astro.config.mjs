3e298c03871fcd672ccf43aa561215f3807e94e8
Shawn Taylor <shawn@mapsandapps.net>
2026-04-14 13:21:18 -0400

    Init Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import db from '@astrojs/db';
+import db from "@astrojs/db";
+import vercelStatic from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  output: "static",
+  adapter: vercelStatic({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  integrations: [db()],
 });
