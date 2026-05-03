e596af431e877dd9dbbc70c22948c5307ab6de42
Shawn Taylor <shawn@mapsandapps.net>
2026-05-03 16:30:31 -0400

    Improve comparison of tune names

---
+++ src/utils/tune-name-comparisons.ts
@@ -0,0 +1,44 @@
+import { sql } from "astro:db";
+
+interface Page {
+  tuneName: string;
+  bookSlug: string;
+}
+
+// for Cooper Book songs, compare *only* the part of the name inside parentheses (if there are parentheses)
+// for all other books, remove anything inside parentheses
+export const getNormalizedCurrentPage = (page: Page) =>
+  page.bookSlug.startsWith("cb-")
+    ? // extract content inside parens, or use full name if no parens
+      (page.tuneName.match(/\(([^)]+)\)/)?.[1]?.trim() ?? page.tuneName)
+    : // strip parens and surrounding spaces
+      page.tuneName.replace(/\s*\([^)]*\)\s*/g, "").trim();
+
+// for Cooper Book songs, compare *only* the part of the name inside parentheses (if there are parentheses)
+// for all other books, remove anything inside parentheses
+export const getNormalizedComparisionPage = (Page: any) => sql<string>`
+  CASE
+    WHEN ${Page.bookSlug} LIKE 'cb-%' THEN
+      CASE
+        WHEN ${Page.tuneName} LIKE '%(%)%'
+        THEN TRIM(SUBSTR(
+          ${Page.tuneName},
+          INSTR(${Page.tuneName}, '(') + 1,
+          INSTR(${Page.tuneName}, ')') - INSTR(${Page.tuneName}, '(') - 1
+        ))
+        ELSE ${Page.tuneName}
+      END
+    ELSE
+      -- strip everything from the first '(' to the matching ')'
+      TRIM(
+        CASE
+          WHEN ${Page.tuneName} LIKE '%(%)%'
+          THEN TRIM(
+            SUBSTR(${Page.tuneName}, 1, INSTR(${Page.tuneName}, '(') - 1) ||
+            SUBSTR(${Page.tuneName}, INSTR(${Page.tuneName}, ')') + 1)
+          )
+          ELSE ${Page.tuneName}
+        END
+      )
+  END
+`;
